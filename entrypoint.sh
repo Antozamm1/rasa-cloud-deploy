7f9c2ac50c63c99c242b3a5e57b539613990a80f
Antozamm1 <134511736+Antozamm1@users.noreply.github.com>
2023-07-20 15:30:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rasa run -m 20230620-132252-horizontal-ambush.tar.gz -p ${PORT} --cors "*"
+# rasa run -m 20230620-132252-horizontal-ambush.tar.gz -p ${PORT} --cors "*"
+
+rasa run -p ${PORT} --cors "*"
